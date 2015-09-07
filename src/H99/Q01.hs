e44dbaec761168022b01e5c12ad825f294d7e40a
Anton Korovin <korovin.anton@gmail.com>
2015-09-07 15:38:45 +0300

    [q01] Refactor.

--- src/H99/Q01.hs
+++ src/H99/Q01.hs
@@ -2,7 +2,7 @@ module H99.Q01 (
   lastElementOf
 ) where
 
-lastElementOf :: (Ord a) => [a] -> a
+lastElementOf :: [a] -> a
 lastElementOf [] = error "'last' of empty list"
-lastElementOf [x] = x
-lastElementOf (head:tail) = lastElementOf tail
+lastElementOf [oneElement] = oneElement
+lastElementOf (_:restOfList) = lastElementOf restOfList
