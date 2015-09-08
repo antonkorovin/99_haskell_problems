9b74f026e7dc5dae47d40a8404c9534462054ce1
Anton Korovin <korovin.anton@gmail.com>
2015-09-08 12:50:56 +0300

    [q03] Added stub for 'elementAt'

---
+++ src/H99/Q03.hs
@@ -0,0 +1,6 @@
+module H99.Q03 (
+  elementAt
+) where
+
+elementAt :: [a] -> Int -> a
+elementAt _ _ = error "Not implemented yet"
---
+++ test/Q03/ElementAtSpec.hs
@@ -0,0 +1,30 @@
+module Q03.ElementAtSpec (spec, main) where
+
+import H99.Q03 (elementAt)
+
+
+import Test.Hspec
+
+import Control.Exception (evaluate)
+
+
+-- `main` is here so that this module can be run from GHCi on its own.  It is
+-- not needed for automatic spec discovery.
+main :: IO ()
+main = hspec spec
+
+
+spec :: Spec
+spec = do
+  describe "elementAt" $ do
+    it "returns k-th element of a list" $ do
+      elementAt ['a'..'z'] 3 `shouldBe` ('c' :: Char)
+
+    it "throws an exception if used index outs of list bounds (from rigth)" $ do
+      evaluate (elementAt [1..10] 11) `shouldThrow` errorCall "Out of bounds"
+
+    it "throws an exception if used index outs of list bounds (from left)" $ do
+      evaluate (elementAt [1..10] 0) `shouldThrow` errorCall "Out of bounds"
+
+    it "throws an exception if used with an empty list" $ do
+      evaluate (elementAt [] 1) `shouldThrow` errorCall "Out of bounds"
