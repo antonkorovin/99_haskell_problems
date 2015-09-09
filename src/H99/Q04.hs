23d7003fece6b2b2c51a38d4a0b4f53347682422
Anton Korovin <korovin.anton@gmail.com>
2015-09-09 11:12:45 +0300

    [q04] Added stub and tests

---
+++ src/H99/Q04.hs
@@ -0,0 +1,6 @@
+module H99.Q04 (
+  listLength
+) where
+
+listLength :: [a] -> Integer
+listLength _ = error "Not implemented yet"
---
+++ test/Q04/ListLengthSpec.hs
@@ -0,0 +1,24 @@
+module Q04.ListLengthSpec (spec, main) where
+
+import Test.Hspec
+
+
+import H99.Q04 (listLength)
+
+-- `main` is here so that this module can be run from GHCi on its own.  It is
+-- not needed for automatic spec discovery.
+main :: IO ()
+main = hspec spec
+
+
+spec :: Spec
+spec = do
+  describe "listLength" $ do
+    it "returns 0 for an empty list" $ do
+        listLength [] `shouldBe` 0
+
+    it "returns 100 for [1..100] list" $ do
+        listLength [1::Int,2..100] `shouldBe` 100
+
+    it "counts count of letters in aphabet" $ do
+     listLength ['a'..'z'] `shouldBe` 26
