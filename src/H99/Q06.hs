9555aeadcecbb76d2fae9686e05d46fecca0ef87
Anton Korovin <korovin.anton@gmail.com>
2015-09-11 15:30:25 +0300

    [q06] Added stub and tests for 'isPalindrome'

---
+++ src/H99/Q06.hs
@@ -0,0 +1,6 @@
+module H99.Q06 (
+  isPalindrome
+) where
+
+isPalindrome :: [a] -> Bool
+isPalindrome _ = False
---
+++ test/Q06/IsPalindromeSpec.hs
@@ -0,0 +1,45 @@
+module Q06.IsPalindromeSpec (spec, main) where
+
+import Test.Hspec
+
+
+import H99.Q06 (isPalindrome)
+
+-- `main` is here so that this module can be run from GHCi on its own.  It is
+-- not needed for automatic spec discovery.
+main :: IO ()
+main = hspec spec
+
+
+spec :: Spec
+spec = do
+  describe "isPalindrome" $ do
+    describe "work with lists" $ do
+      describe "detecting palindrome" $ do
+        it "returns True for an empty list" $ do
+          isPalindrome [] `shouldBe` True
+
+        it "returns True for an one element list" $ do
+            isPalindrome ['a'] `shouldBe` True
+
+
+        it "returns True for a 2 element palindrome list" $ do
+            isPalindrome ['a', 'a'] `shouldBe` True
+
+        it "returns True for a 3 element palindrome list" $ do
+            isPalindrome ['a', 'b', 'a'] `shouldBe` True
+
+
+    describe "work with strings" $ do
+      describe "detecting palindrome" $ do
+        it "returns True for an empty string" $ do
+            isPalindrome "" `shouldBe` True
+
+        it "returns True for an one character string" $ do
+            isPalindrome "a" `shouldBe` True
+
+        it "returns True for a 2 characters palindrome string" $ do
+            isPalindrome "aa" `shouldBe` True
+
+        it "returns True for a 3 characters palindrome string" $ do
+            isPalindrome "aba" `shouldBe` True
