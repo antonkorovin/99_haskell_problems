d7ebf5a66fcc9080c9ce11f1c27daeddf8ee39f0
Anton Korovin <korovin.anton@gmail.com>
2015-09-16 13:22:30 +0300

    [q04] Implement 'listLength' using foldl

--- src/H99/Q04.hs
+++ src/H99/Q04.hs
@@ -1,7 +1,11 @@
 module H99.Q04 (
-  listLength
+  listLength,
+  listLengthUsingFold
 ) where
 
 listLength :: [a] -> Integer
 listLength [] = 0
 listLength (_ : xs) = 1 + listLength xs
+
+listLengthUsingFold :: [a] -> Integer
+listLengthUsingFold list = foldl (\acc _ -> acc + 1) 0 list
--- test/Q04/ListLengthSpec.hs
+++ test/Q04/ListLengthSpec.hs
@@ -3,7 +3,7 @@ module Q04.ListLengthSpec (spec, main) where
 import Test.Hspec
 
 
-import H99.Q04 (listLength)
+import H99.Q04 (listLength, listLengthUsingFold)
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
 -- not needed for automatic spec discovery.
@@ -22,3 +22,13 @@ spec = do
 
     it "counts count of letters in aphabet" $ do
      listLength ['a'..'z'] `shouldBe` 26
+
+  describe "listLengthUsingFold" $ do
+    it "returns 0 for an empty list" $ do
+        listLengthUsingFold [] `shouldBe` 0
+
+    it "returns 100 for [1..100] list" $ do
+        listLengthUsingFold [1::Int,2..100] `shouldBe` 100
+
+    it "counts count of letters in aphabet" $ do
+     listLengthUsingFold ['a'..'z'] `shouldBe` 26
