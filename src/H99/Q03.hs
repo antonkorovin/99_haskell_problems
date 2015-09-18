7012c1126a5c8137f369afd7c08bb7556c12a0e6
Anton Korovin <korovin.anton@gmail.com>
2015-09-18 08:15:22 +0300

    [q03] Added task description

--- src/H99/Q03.hs
+++ src/H99/Q03.hs
@@ -2,6 +2,21 @@ module H99.Q03 (
   elementAt
 ) where
 
+-- Problem 3
+-- (*) Find the K'th element of a list. The first element in the list is number 1.
+
+-- Example:
+
+-- * (element-at '(a b c d e) 3)
+-- c
+-- Example in Haskell:
+
+-- Prelude> elementAt [1,2,3] 2
+-- 2
+-- Prelude> elementAt "haskell" 5
+-- 'e'
+
+
 elementAt :: [a] -> Int -> a
 elementAt _ 0 = error "Out of bounds"
 elementAt ([]) _ = error "Out of bounds"
