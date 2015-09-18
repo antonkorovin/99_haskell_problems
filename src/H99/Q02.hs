42e711c9ccd3c806751bbb499c7f585effce0b8f
Anton Korovin <korovin.anton@gmail.com>
2015-09-18 08:15:15 +0300

    [q02] Added task description

--- src/H99/Q02.hs
+++ src/H99/Q02.hs
@@ -2,6 +2,19 @@ module H99.Q02 (
   lastButOneOf
 ) where
 
+
+-- Problem 2
+-- (*) Find the last but one element of a list.
+
+-- (Note that the Lisp transcription of this problem is incorrect.)
+
+-- Example in Haskell:
+
+-- Prelude> myButLast [1,2,3,4]
+-- 3
+-- Prelude> myButLast ['a'..'z']
+-- 'y'
+
 lastButOneOf :: [a] -> a
 lastButOneOf [] = error "Out of bounds"
 lastButOneOf (lastButOneElement : _ : []) = lastButOneElement
