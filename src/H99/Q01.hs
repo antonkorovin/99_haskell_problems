3fa20896db27007e5b17502497c89c7220a4b475
Anton Korovin <korovin.anton@gmail.com>
2015-09-18 08:14:51 +0300

    [q01] Added task description

--- src/H99/Q01.hs
+++ src/H99/Q01.hs
@@ -3,6 +3,18 @@ module H99.Q01 (
   lastElementOfUsingFold
 ) where
 
+-- Problem 1
+-- (*) Find the last element of a list.
+
+-- (Note that the Lisp transcription of this problem is incorrect.)
+
+-- Example in Haskell:
+
+-- Prelude> myLast [1,2,3,4]
+-- 4
+-- Prelude> myLast ['x','y','z']
+-- 'z'
+
 lastElementOf :: [a] -> a
 lastElementOf [] = error "Out of bounds"
 lastElementOf [oneElement] = oneElement
