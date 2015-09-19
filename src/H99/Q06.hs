d74ad642ddddf3b4c9ea5dbfbe0515f18e02f408
Anton Korovin <korovin.anton@gmail.com>
2015-09-20 00:01:46 +0300

    [q06] Added task description

--- src/H99/Q06.hs
+++ src/H99/Q06.hs
@@ -4,5 +4,17 @@ module H99.Q06 (
 
 import H99.Q05 (reverseList)
 
+-- Problem 6
+-- (*) Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x).
+
+-- Example in Haskell:
+
+-- *Main> isPalindrome [1,2,3]
+-- False
+-- *Main> isPalindrome "madamimadam"
+-- True
+-- *Main> isPalindrome [1,2,4,8,16,8,4,2,1]
+-- True
+
 isPalindrome :: Ord a => [a] -> Bool
 isPalindrome x = (reverseList x == x)
