c6bcc60e1fcb87c79e323e16b81aa49790ef31ad
Anton Korovin <korovin.anton@gmail.com>
2015-09-19 23:54:01 +0300

    [q05] Added task description

--- src/H99/Q05.hs
+++ src/H99/Q05.hs
@@ -3,6 +3,17 @@ module H99.Q05 (
   reverseListUsingFold
 ) where
 
+
+-- Problem 5
+-- (*) Reverse a list.
+
+-- Example in Haskell:
+
+-- Prelude> myReverse "A man, a plan, a canal, panama!"
+-- "!amanap ,lanac a ,nalp a ,nam A"
+-- Prelude> myReverse [1,2,3,4]
+-- [4,3,2,1]
+
 reverseList :: [a] -> [a]
 reverseList xs = reverseListImpl [] xs where
     reverseListImpl :: [a] -> [a] -> [a]
