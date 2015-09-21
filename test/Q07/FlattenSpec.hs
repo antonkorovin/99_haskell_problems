cc9ca7e3e31283e4f9969f1f20b1f483a1961810
Anton Korovin <korovin.anton@gmail.com>
2015-09-21 19:33:19 +0300

    [q07] Added test specification.

--- test/Q07/FlattenSpec.hs
+++ test/Q07/FlattenSpec.hs
@@ -4,7 +4,6 @@ import Test.Hspec
 
 
 import H99.Q07
---(flatten, NestedList(List))
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
 -- not needed for automatic spec discovery.
@@ -12,8 +11,20 @@ main :: IO ()
 main = hspec spec
 
 
+anEmptyList :: [()]
+anEmptyList = ([] :: [()])
+
+anEmptyNestedList :: NestedList ()
+anEmptyNestedList = List [] :: NestedList ()
+
 spec :: Spec
 spec = do
   describe "flatten" $ do
     it "returns the same for an empty list" $ do
+        flatten anEmptyNestedList `shouldBe` anEmptyList
+
+    it "returns list with one element for 'Elem'" $ do
         flatten (Elem 'a') `shouldBe` ['a']
+
+    it "returns flatten list for multy-level list" $ do
+        flatten (List [Elem '1', List [Elem '2', List [Elem '3', Elem '4'], Elem '5']]) `shouldBe` ['1', '2', '3', '4', '5']
