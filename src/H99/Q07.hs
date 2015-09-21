ffbada73c931af062249f2d94b8cce7f41a1b3ce
Anton Korovin <korovin.anton@gmail.com>
2015-09-22 00:15:00 +0300

    [q07] Implement flatten using foldl.

--- src/H99/Q07.hs
+++ src/H99/Q07.hs
@@ -1,6 +1,7 @@
 module H99.Q07
 (   NestedList(Elem, List)
 ,   flatten
+,   flattenUsingFoldl
 ) where
 
 
@@ -34,4 +35,11 @@ flatten :: Ord a => NestedList a -> [a]
 flatten (List []) = []
 flatten (Elem e) = [e]
 flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+
+
+
+flattenUsingFoldl :: Ord a => NestedList a -> [a]
+flattenUsingFoldl (List []) = []
+flattenUsingFoldl (Elem e) = [e]
+flattenUsingFoldl (List nestedList) =  foldl (\alreadyFlat x -> alreadyFlat ++ flatten x) [] nestedList
 
--- test/Q07/FlattenSpec.hs
+++ test/Q07/FlattenSpec.hs
@@ -28,3 +28,14 @@ spec = do
 
     it "returns flatten list for multy-level list" $ do
         flatten (List [Elem '1', List [Elem '2', List [Elem '3', Elem '4'], Elem '5']]) `shouldBe` ['1', '2', '3', '4', '5']
+
+  describe "flattenUsingFoldl" $ do
+    it "returns the same for an empty list" $ do
+        flattenUsingFoldl anEmptyNestedList `shouldBe` anEmptyList
+
+    it "returns list with one element for 'Elem'" $ do
+        flattenUsingFoldl (Elem 'a') `shouldBe` ['a']
+
+    it "returns flatten list for multy-level list" $ do
+        flattenUsingFoldl (List [Elem '1', List [Elem '2', List [Elem '3', Elem '4'], Elem '5']]) `shouldBe` ['1', '2', '3', '4', '5']
+
